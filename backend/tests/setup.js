c6dd1de6f2d4d08d346f482c7f47e66370741b49
Wally A Silva <wallyson-s@hotmail.com>
2024-12-12 22:09:01 +0000

    Test husky 1 more time

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -11,7 +11,6 @@ beforeAll(async () => {
         await mongoose.disconnect();
         await mongoose.connect(process.env.MONGODB_URI_TEST);
         console.log('Connected to test database');
-        console.log('test');
     } catch (error) {
         console.error('Error connecting to test database:', error);
         process.exit(1);
